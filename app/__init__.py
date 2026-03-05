21971c89815a900cd28983c7fc0ea8862cc46f85
Michael Perez <michael.perez1011+ffhtchki@gmail.com>
2026-03-05 12:42:13 -0500

    Add app package init with health check import

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,10 @@
-﻿# War Machine - Main Application Package
+"""
+War Machine Application Package
+
+Centralized imports for all War Machine subsystems.
+"""
+
+# Make health_check easily accessible
+from .health_check import perform_health_check, print_session_info
+
+__all__ = ['perform_health_check', 'print_session_info']
