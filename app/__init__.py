566084a813830be2358df98a304dadb923666730
Michael Perez <michael.perez1011+ffhtchki@gmail.com>
2026-03-14 13:32:26 -0400

    fix: remove broken health_check import from app/__init__.py (module deleted)

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,5 @@
 """
 War Machine Application Package
-
-Centralized imports for all War Machine subsystems.
 """
 
-# Make health_check easily accessible
-from .health_check import perform_health_check, print_session_info
-
-__all__ = ['perform_health_check', 'print_session_info']
+__all__ = []
